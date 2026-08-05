ff2ccacfe470e4b9b1b99a834fa3396982ef65a6
Trung606 <delingthing@gmail.com>
2026-08-05 20:18:32 +0700

    .

--- FileSharingAPI/Services/FileCleanupService.cs
+++ FileSharingAPI/Services/FileCleanupService.cs
@@ -16,7 +16,7 @@ namespace FileSharingAPI.Services
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            using var timer = new PeriodicTimer(TimeSpan.FromMinutes(2));
+            using var timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
 
             while (await timer.WaitForNextTickAsync(stoppingToken))
             {
